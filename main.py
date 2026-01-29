472607d054b43911fb42bb21e9424b3c559915ca
Ryan Ng Xu Yang <ryanngxuyang@Ryans-MacBook-Air.local>
2026-01-29 15:25:54 +0800

    Update greeting to Hello You

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World"  # We will change this later!
+    return "Hello you"  # We will change this later!
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
